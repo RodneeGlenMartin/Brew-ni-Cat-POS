068c3c0fc91926012ad116a40d268294185d0a4e
Rodnee Glen Martin <s.rgamartin@usm.edu.ph>
2026-06-06 15:36:22 +0800

    fix: Re-add destructive migration fallback to override cloud auto-backup loops

--- app/src/main/java/com/example/cattasticpos/data/local/PosDatabase.kt
+++ app/src/main/java/com/example/cattasticpos/data/local/PosDatabase.kt
@@ -57,6 +57,7 @@ abstract class PosDatabase : RoomDatabase() {
                     "pos_database"
                 )
                 .addMigrations(MIGRATION_9_10)
+                .fallbackToDestructiveMigration()
                 .addCallback(PosDatabaseCallback(scope))
                 .build()
                 INSTANCE = instance
