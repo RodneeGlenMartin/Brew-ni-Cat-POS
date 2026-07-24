6685326fe525c479c4cfbfa772905cb9adaae79d
Rodnee Glen Martin <rodneeglenmartin@gmail.com>
2026-07-24 20:40:47 +0800

    Reset versionCode to 10119 matching release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.example.cattasticpos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10120
+        versionCode = 10119
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
